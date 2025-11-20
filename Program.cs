25e891e225d79259251c632318b5d9af18163115
Angelinaleskovets <leskovets_ak24@nuwm.edu.ua>
2025-11-20 14:27:37 +0200

    qwertryq

--- Program.cs
+++ Program.cs
@@ -1,24 +1,60 @@
-﻿using System;
-
-namespace LabWork
+﻿using System; //1
+class Sphere
 {
-    // Даний проект є шаблоном для виконання лабораторних робіт
-    // з курсу "Об'єктно-орієнтоване програмування та патерни проектування"
-    // Необхідно змінювати і дописувати код лише в цьому проекті
-    // Відео-інструкції щодо роботи з github можна переглянути 
-    // за посиланням https://www.youtube.com/@ViktorZhukovskyy/videos 
+    public double Radius { get; }
+    public double CenterX { get; }
+    public double CenterY { get; }
+    public double CenterZ { get; }
 
-    class Result
-    { 
-    // TODO: do it !
-    }
-    
-    class Program
+    public Sphere(double radius, double x, double y, double z)
     {
-        static void Main(string[] args)
+        Radius = radius;
+        CenterX = x;
+        CenterY = y;
+        CenterZ = z;
+    }
+
+    //git hjgjgh
+    // Метод для перевірки, чи точка потрапляє в кулю.
+    public bool IsPointInside(double x, double y, double z)
+    {
+        double dx = x - CenterX;
+        double dy = y - CenterY;
+        double dz = z - CenterZ;
+        double distanceSquared = dx * dx + dy * dy + dz * dz;
+        return distanceSquared <= Radius * Radius;
+    }
+}
+
+class Program
+{
+    static void Main()  //check
+    {
+        // Створення об'єкта кулі.
+        Sphere sphere = new Sphere(5.0, 0.0, 0.0, 0.0); // Радіус 5, центр (0, 0, 0)
+
+        // Введення координат точки
+        Console.WriteLine("Введіть координати точки (x, y, z):");
+        Console.Write("x = ");
+        double x = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("y = ");
+        double y = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("z = ");
+        double z = Convert.ToDouble(Console.ReadLine());
+
+        // Перевірка, чи точка всередині кулі
+        bool inside = sphere.IsPointInside(x, y, z);
+
+        if (inside)
         {
-            
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Точка знаходиться всередині кулі або на її поверхні.");
+        }
+        else
+        {
+            Console.WriteLine("Точка знаходиться поза кулею.");
         }
     }
 }
+
